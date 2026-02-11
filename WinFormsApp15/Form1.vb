8a7a4d634870837eea3ef5491ed2a0c6e3ec11dc
Kryomenos <ioslm771@gmail.com>
2026-02-11 23:58:04 +0300

    Update Form1.vb

--- WinFormsApp15/Form1.vb
+++ WinFormsApp15/Form1.vb
@@ -1,6 +1,6 @@
 ﻿Public Class Form1
 
-    ' تأكد أن هذه المتغيرات معرفة كـ Button
+    
     Dim FirstClicked As Button = Nothing
     Dim SecondClicked As Button = Nothing
 
@@ -8,10 +8,10 @@
 
         Dim btn As Button = CType(sender, Button)
 
-        ' منع الضغط على نفس الزر مرتين أو الضغط أثناء عمل المؤقت
+    
         If Timer1.Enabled OrElse btn Is FirstClicked Then Return
 
-        ' إظهار الصورة وإخفاء الرقم
+        
         btn.Image = ImageList1.Images(CInt(btn.Tag))
         btn.Text = ""
 
@@ -22,13 +22,13 @@
 
         SecondClicked = btn
 
-        ' مقارنة الـ Tag لمعرفة هل الصور متطابقة
+        
         If FirstClicked.Tag.ToString() = SecondClicked.Tag.ToString() Then
-            ' في حال التطابق: نترك الصور ظاهرة ونفرغ المتغيرات للاستمرار
+        
             FirstClicked = Nothing
             SecondClicked = Nothing
         Else
-            ' في حال عدم التطابق: نشغل المؤقت لإخفائهم بعد ثانية
+            
             Timer1.Start()
         End If
     End Sub
@@ -36,14 +36,15 @@
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
         Timer1.Stop()
 
-        ' إعادة الأرقام وإخفاء الصور
+        
         FirstClicked.Text = (CInt(FirstClicked.Tag) + 1).ToString()
         SecondClicked.Text = (CInt(SecondClicked.Tag) + 1).ToString()
 
         FirstClicked.Image = Nothing
         SecondClicked.Image = Nothing
 
-        ' تفريغ المتغيرات لجولة جديدة
+
+        
         FirstClicked = Nothing
         SecondClicked = Nothing
     End Sub
